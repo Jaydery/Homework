c6b2c90a757ee675b030690899aa4c063d3e5309
Jaydery <99312938+Jaydery@users.noreply.github.com>
2024-07-22 17:42:43 +0500

    Update 17.7.1.py

--- 17.7.1.py
+++ 17.7.1.py
@@ -1,9 +1,6 @@
-for i in range (1,10):
+multipl = 1
+for i in range(1, 10):
    if i % 2 == 0:
-      print()
       continue
-   for j in range (1,10):
-      if j % 2 == 0:
-         continue
-      multipl = i * j
-      print(multipl)
+   multipl *=i
+print(multipl)
